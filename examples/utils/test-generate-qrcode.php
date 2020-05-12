2cce8fbd56880b73f61266cfc20d4c336e557832
wenqidong <wqd1688@gmail.com>
2020-05-12 16:14:26 +0800

    生成二维码

--- examples/utils/test-generate-qrcode.php
+++ examples/utils/test-generate-qrcode.php
@@ -1,1 +1,20 @@
 <?php
+
+use SayaCloud\Api\Utils\QRCode;
+use SayaCloud\SayaCloud;
+
+include '../../vendor/autoload.php';
+include '../config.php';
+
+try {
+    $client = SayaCloud::client($config);
+
+    $qrcode = new QRCode();
+    $qrcode->setString('http://www.baidu.com?keyword=123&name=cc');
+
+    $result = $client->request($qrcode);
+    var_dump($result);
+
+} catch (Exception $e) {
+    var_dump($e->getMessage());
+}
--- src/api/utils/QRCode.php
+++ src/api/utils/QRCode.php
@@ -1,1 +1,27 @@
 <?php
+
+namespace SayaCloud\Api\Utils;
+
+use SayaCloud\Contract\RequestAbstract;
+use SayaCloud\Lib\RequestMethod;
+
+class QRCode extends RequestAbstract
+{
+    protected $requestMethod = RequestMethod::GET;
+
+    protected $apiUri = '/qrcode/url';
+
+    /**
+     * 需要转换为QRCode的字符串
+     * @var string
+     */
+    protected $string;
+
+    /**
+     * @param string $string
+     */
+    public function setString(string $string): void
+    {
+        $this->string = $string;
+    }
+}
